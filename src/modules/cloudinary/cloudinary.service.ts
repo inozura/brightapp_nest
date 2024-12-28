c38cee33c260ee6be2c24a346bf568d807b8eec6
Novandra Zulfi Ramadhan <nodhzen@gmail.com>
2024-12-28 08:20:45 +0700

    fix: prettier cloudinary.service

--- src/modules/cloudinary/cloudinary.service.ts
+++ src/modules/cloudinary/cloudinary.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import toStream = require('buffer-to-stream');
+
 @Injectable()
 export class CloudinaryService {
   async uploadImage(
@@ -9,6 +10,7 @@ export class CloudinaryService {
     return new Promise((resolve, reject) => {
       const upload = v2.uploader.upload_stream((error, result) => {
         if (error) return reject(error);
+
         resolve(result);
       });
 
