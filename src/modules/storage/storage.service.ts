f569fce7928999f8d2cb11032c427e198cbf11b0
Novandra Zulfi Ramadhan <nodhzen@gmail.com>
2024-12-28 08:19:33 +0700

    feat: supported couldinary storage

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -1,36 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { createWriteStream, unlink, readFile } from 'fs';
+import { SaveImageRequestDTO } from './dtos/request/storage-request.dto';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 
 @Injectable()
 export class StorageService {
+  constructor(private cloudinaryService: CloudinaryService) {}
+
   async saveImage(
     file: Express.Multer.File,
     userId: string,
     userImage?: string,
+    type?: 'local' | 'cloudinary',
   ): Promise<string> {
-    const filename = `${userId}-${Date.now()}-${file.originalname}`;
-    const path = `uploads/${filename}`;
+    switch (type) {
+      case 'cloudinary': {
+        const clodinaryRes = await this.cloudinaryService.uploadImage(file);
+        return clodinaryRes.original_filename;
+      }
 
-    if (!file && !file.buffer && !file.originalname) {
-      throw new Error('file corrupted');
-    }
-    if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
-      throw new Error('file type mismatch');
-    }
+      default: {
+        const filename = `${userId}-${Date.now()}-${file.originalname}`;
+        const path = `uploads/${filename}`;
 
-    // Check if the file already exists
-    if (userImage) {
-      unlink(`uploads/${userImage}`, () => {
-        console.log('file was deleted');
-      });
-    }
+        if (!file && !file.buffer && !file.originalname) {
+          throw new Error('file corrupted');
+        }
+        if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
+          throw new Error('file type mismatch');
+        }
 
-    return new Promise((resolve, reject) =>
-      createWriteStream(path)
-        .end(file.buffer)
-        .on('finish', () => resolve(filename))
-        .on('error', (error) => reject(error)),
-    );
+        // Check if the file already exists
+        if (userImage) {
+          unlink(`uploads/${userImage}`, () => {
+            console.log('file was deleted');
+          });
+        }
+
+        return new Promise((resolve, reject) =>
+          createWriteStream(path)
+            .end(file.buffer)
+            .on('finish', () => resolve(filename))
+            .on('error', (error) => reject(error)),
+        );
+      }
+    }
   }
 
   async getImagePath(filename: string): Promise<Buffer | null> {
